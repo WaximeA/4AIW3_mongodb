a9967c3d77c6bc83d2390e649fbd0d7553f4f00c
waxime <aveline.maxime@gmail.com>
2019-04-26 11:38:22 +0200

    WIP manage add user and movies

--- server/app.js
+++ server/app.js
@@ -1,160 +1,40 @@
 console.log('In app.js');
 
-const mongoose = require('mongoose');
-const bcryptjs = require('bcryptjs');
+const db = require('./lib/db');
+const User = require('./models/user');
+const Movie = require('./models/movie');
 
-// HTTP connexion like to mongoose
-mongoose.connect(`mongodb://mongo`, {
-  user: process.env.MONGODB_USER,
-  pass: process.env.MONGODB_PASS,
-  dbName: process.env.MONGODB_DBNAME,
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
-
-const db = mongoose.connection;
-
-db.once('open', () => console.log('Connected to mongoose'));
-db.on('error', () => console.log('Error'));
-
-// *************** USERS ***************
-
-// User schema
-const UserSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: 'First name is required',
-  },
-  lastName: {
-    type: String,
-    required: 'Last name is required',
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: 'Email address is required',
-    // validate: [validateEmail, 'Please fill a valid email address'], // Same that the match param
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-  },
-  rgpd_terms: {
-    type: Boolean,
-    required: 'Please accept rgpd therms',
-  },
-  password: {
-    type: String,
-    required: 'Password is required',
-  },
-  createdAt: Date
-});
-
-// Listener avant le save
-UserSchema.pre('save', function (next) {
-  console.log('saving ' + this.firstName + ' ' + this.lastName);
-  let salt = bcryptjs.genSaltSync(10);
-  let hash = bcryptjs.hashSync(this.password, salt);
-  this.password = hash;
-  next();
-});
-
-// Listener après le save
-UserSchema.post('save', function() {
-  console.log(this.firstName + ' ' + this.lastName + ' saved');
-  console.log(this);
-});
-
-// User collection
-const User = mongoose.model('User', UserSchema);
-
-// Insert user
+// *************** USER DATA **************
 db.once('open', () => {
-  // Define data
   const firstUser = new User({
-    firstName: 'Toto',
-    lastName: 'Tot',
-    email: 'toto@tot.fr',
+    firstName: 'Waxime',
+    lastName: 'Aveline',
+    email: 'waxime@aveline.fr',
     rgpd_terms: true,
-    password: 'toto123',
+    password: 'wax123',
     createdAt: Date.now(),
   });
-  const secondUser = new User({
-    firstName: 'Tata',
-    lastName: 'Tat',
-    email: 'tata@tat.fr',
-    rgpd_terms: true,
-    password: 'tata123',
-    createdAt: Date.now(),
-  });
-  const thirdUser = new User({
-    firstName: 'Titi',
-    lastName: 'tit',
-    email: 'titi@tit.fr',
-    rgpd_terms: true,
-    password: 'tata123',
-    createdAt: Date.now() ,
-  });
+  // const secondUser = new User({
+  //   firstName: 'John',
+  //   lastName: 'Doe',
+  //   email: 'john@doe.fr',
+  //   rgpd_terms: true,
+  //   password: 'john123',
+  //   createdAt: Date.now(),
+  // });
 
   // Save data
-  firstUser.save().then(data =>
-      console.log(data)
-
-  ).catch(error =>
-      console.log(error)
-  );
-  secondUser.save().then(data =>
-      console.log(data)
-
-  ).catch(error =>
-      console.log(error)
-  );
-  thirdUser.save().then(data =>
-      console.log(data)
-
-  ).catch(error =>
-      console.log(error)
-  );
+  firstUser.register().then(data => {
+    console.log(data);
+    User.login(firstUser.email, firstUser.password).
+        then(user => console.log(user)).
+        catch(error => console.log(error));
+  })
+  .catch(error => console.log(error));
 });
 
 
-// *************** MOVIES ***************
-
-const MovieSchema = mongoose.Schema({
-  title: String,
-  createdAt: Date,
-  year: {
-    type: Number,
-    required: true,
-    min: 1900
-  },
-  category: {
-    type: String,
-    enum: ['Horror', 'SF', 'Drama', 'Comedy', 'Animation']
-  }
-});
-
-// Création d'une méthode lié au schéma qui vérifie si le film est à l'affiche
-MovieSchema.methods.onScreen = function() {
- return Date.now() > new Date (`${this.year}`)
-}
-
-// Listener avant le save
-MovieSchema.pre('save', function(next) {
-  console.log('Saving '+ this.title);
-  console.log(this);
-  next();
-});
-
-// Listener après le save
-MovieSchema.post('save', function() {
-  console.log(this.title + ' saved');
-  console.log(this);
-});
-
-// Model collection
-const Movie = mongoose.model('Movie', MovieSchema);
-
-// Insert data
+// *************** MOVIE DATA **************
 db.once('open', () => {
   const firstMovie = new Movie({
     title: 'Titanic',
@@ -162,27 +42,12 @@ db.once('open', () => {
     year: 1997,
     category: 'Drama'
   });
-
-  firstMovie.save().then(
-      data => console.log(data)
-  ).catch(
-      error => console.log(error)
-  );
-
   const secondMovie = new Movie({
     title: 'Toy Story 4',
     createdAt: new Date('2019-05-01'),
     year: 2019,
     category: 'Animation'
   });
-
-  secondMovie.save().then(data =>
-      console.log(data.onScreen)
-
-  ).catch(error =>
-      console.log(error)
-  );
-
   const thirdMovie = new Movie({
     title: 'Harry potter',
     createdAt: new Date('2004-05-01'),
@@ -190,17 +55,10 @@ db.once('open', () => {
     category: 'SF'
   });
 
-  thirdMovie.save().then(data =>
-      console.log(data.onScreen)
-
-  ).catch(error =>
-      console.log(error)
-  );
+  // firstMovie.save().then(data => console.log(data.onScreen)).catch(error => console.log(error));
+  // secondMovie.save().then(data => console.log(data.onScreen)).catch(error => console.log(error));
+  // thirdMovie.save().then(data => console.log(data.onScreen)).catch(error => console.log(error));
 
 });
 
-db.once('open', () => {
-  Movie.find().then(data => console.log(data))
-  User.find().then(data => console.log(data))
-});
 db.on('error', (error) => console.log(error, 'Error'));
