907546ec3c3f57ba5b7e287a06b55384005b6d4c
waxime <aveline.maxime@gmail.com>
2019-04-26 12:26:37 +0200

    add movie post route with status

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -4,7 +4,23 @@ const Movie = require('../models/movie');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  Movie.find(req.query).then(data => res.json(data));
+  Movie.find(req.query)
+  .then(data => res.json(data))
+  .catch(error => res.sendStatus(500));
+});
+
+router.post('/', (req, res) => {
+  const movie = new Movie(req.body);
+  movie.save()
+  .then(data => res.status(201).json(data))
+  .catch(error => {
+    if (error.name = "ValidationError") {
+      res.status(400).json(error)
+    } else {
+      res.status(500);
+    }
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
