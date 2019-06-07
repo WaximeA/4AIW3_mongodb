75989968511157ffd874b4d68705091cb441460d
waxime <aveline.maxime@gmail.com>
2019-06-07 10:37:00 +0200

    Transform todoItem into stateless component

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,17 +1,11 @@
 import React from "react";
 import TodoItemActions from './TodoItemActions';
 
-class TodoItem extends React.Component {
-  render() {
-    const { item, onSelect, onDelete, ...rest } = this.props;
-
-    return <li >
+const TodoItem = ({item, onSelect, onDelete}) => <li>
         <div style={{ opacity: item.checked ? 0.5 : 1 }} onClick={() => onSelect(item)}>
           {item.text}
         </div>
         <TodoItemActions onSelect={() => onSelect(item)} onDelete={() => onDelete(item)}/>
-      </li>
-  }
-}
+</li>;
 
 export default TodoItem;
