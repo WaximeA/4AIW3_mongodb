e9b756d1b517994e6db41460bcc0ac63d659c8fc
waxime <aveline.maxime@gmail.com>
2019-06-28 12:06:59 +0200

    Use hooks to update data when todo list updated

--- client/src/components/Todo/TodoList.js
+++ client/src/components/Todo/TodoList.js
@@ -1,55 +1,22 @@
-import React from "react";
+import React, {useContext, useEffect, useMemo} from "react";
 import TodoItem from "./TodoItem"
 import TodoForm from './TodoForm';
 import TodoContext from '../../context/TodoContext';
 
-class TodoList extends React.Component {
-  static contextType = TodoContext;
+const TodoList = (props) =>  {
+  const context = useContext(TodoContext);
 
-  newTodo = (text) => {
-    const newTodo = {text, checked: false};
+  useEffect(() => {
+    context.loadTodos();
+  });
 
-    // set new state to handle the rendering and add newTodo at the end of state
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    })
-  }
-
-  changeTodo = (item) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.text === item.text) {
-          todo.checked = !todo.checked;
-        }
-
-        return todo;
-      })
-    })
-  }
-  deleteTodo = (item) => {
-    this.setState({
-      todos: this.state.todos.filter(todo => {
-        return todo.text !== item.text;
-      })
-    })
-  }
-
-  componentDidMount() {
-    this.context.loadTodos();
-  }
-
-  render() {
-    return <TodoContext.Consumer >
-      {(state) => <ul>
-        {
-          state.todos.map((item, index) =>
-              <TodoItem key={index} item={item} />
-          )
-        }
-      </ul>}
-    </TodoContext.Consumer>
-
-  }
+  return useMemo(() => <ul>
+      {
+        this.context.todos.map((item, index) =>
+            <TodoItem key={index} item={item} />
+        )
+      }
+    </ul>, [context.todos]);
 }
 
 export default TodoList;
